7d8677f61160807c92f77beb0e5b7d958d22182b
UnoMeDD <Dominic.Duda@gmx.at>
2023-01-18 20:54:22 +0100

    YOLOv3 Commit
    YOLOv3 Commit

---
+++ YOLOv3/loadYOLO.py
@@ -0,0 +1,15 @@
+import cv2
+import numpy as np
+
+# Load Yolo
+print("LOADING YOLO")
+net=cv2.dnn.readNet("yolov3.weights","yolov3.cfg")
+#save all the names in file o the list classes
+classes = []
+with open("coco.names", "r") as f:
+    classes = [line.strip() for line in f.readlines()]
+#get layers of the network
+layer_names = net.getLayerNames()
+#Determine the output layer names from the YOLO model 
+output_layers = [layer_names[i-1] for i in net.getUnconnectedOutLayers()]
+print("YOLO LOADED")
